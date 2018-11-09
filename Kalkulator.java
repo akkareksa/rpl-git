fba2f43c3a614c0b2cae85f629d560ec41a37f4b
MUHAMMAD RAVI <i16041@ftis>
2018-11-09 10:23:39 +0700

    min max

--- Kalkulator.java
+++ Kalkulator.java
@@ -3,33 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package calculator;
 
 /**
  *
- * @author i16038
+ * @author i16041
  */
-public class Kalkulator {
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public float plus(float a, float b){
+        return a+b;
+        
+    }
+    public float minus(float a, float b){
+        return a-b;
         
     }
     
-    public float plus(float a,float b){
-        
-        
-    }
-    
-    public float minus(float a,float b){
-        
-        
-    }
-    
-    public float multiply(float a,float b){
-        
+    public float multiplication(float a, float b){
+        return a*b;
         
     }
     
     public float division(float a,float b){
-        
+        return a/b;
         
     }
+	
+	public float max(float a, float b){
+		return max(a,b);
+    }
+	
+	public float min(float a, float b){
+		return mint(a,b);
+    
 }
